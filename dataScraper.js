5171285348b4250e06729543b1b5e044e452bf65
Austin Lindenberg <austindlindenber@ufl.edu>
2016-12-04 00:08:47 -0500

    Working web scraper using casper, currently hard-coded with Barcelona 2016. Need to add functionality for doing all teams/year that we intend to at once

---
+++ dataScraper.js
@@ -0,0 +1,157 @@
+var casper = require('casper').create();
+casper.start('http://www.espnfc.us/club/barcelona/83/squad');
+
+// Create arrays to hold the strings that will be formatted tuples for insertion into the respective table
+var goalieTuples = [];
+var outfielderTuples = [];
+var playerTuples = [];
+
+casper.then(function() {
+    var team = this.getElementInfo('.squad-title h1').text;
+    var year = this.getElementInfo('#squad-seasons-dropdown span').text.substring(0,4);
+    var dataTables = this.getElementsInfo('.responsive-table-content tr');
+    for(i = 0; i < (dataTables.length); i++) {
+        var unparsedString = dataTables[i].text;
+        var parsedString = [];
+        for(j = 0; j < unparsedString.length; j++) {
+            var character = unparsedString.charAt(j);
+            if(character != '\t' && character != '\n' && character != ' ') {
+                var str = '';
+                while(character != '\t' && character != '\n' && character != ' ') {
+                    str += character;
+                    j++;
+                    character = unparsedString.charAt(j);
+                }
+                if(str == "--")
+                    str = 0;
+                // Check for the rare case of a player not having a last name. Add an empty string if that's the case
+                if(parsedString.length == 3 && !isNaN(str)) {
+                   parsedString.push('');
+                }
+                parsedString.push(str);
+            }
+        }
+        if(parsedString[0] == 'G') {
+            var goalieQueries = parseGoalies(parsedString, year, team);
+            // Break the results into two strings: one for GOALIES table insertion and one for PLAYER table insertion
+            var goalieTable = goalieQueries[0];
+            var playerTable = goalieQueries[1];
+            goalieTuples.push(goalieTable);
+            playerTuples.push(playerTable);
+        } 
+        else if(parsedString[0] == 'M' || parsedString[0] == 'F' || parsedString[0] == 'D') {
+            var outfieldQueries = parseOutfielders(parsedString, year, team);
+            // Break the results into two strings: one for OUTFIELDERS table insertion and one for PLAYER table insertion
+            var outfielderTable = outfieldQueries[0];
+            var playerTable = outfieldQueries[1];
+            outfielderTuples.push(outfielderTable);
+            playerTuples.push(playerTable);           
+        }
+    }
+    console.log('*******GOALIE TUPLES*******')
+    for(i = 0; i < goalieTuples.length; i++) 
+        console.log(goalieTuples[i]);
+    console.log('\n *******OUTFIELDERS TUPLES*******')
+    for(i = 0; i < outfielderTuples.length; i++) 
+        console.log(outfielderTuples[i]);
+    console.log('\n *******PLAYER TUPLES*******')
+    for(i = 0; i < playerTuples.length; i++) 
+        console.log(playerTuples[i]);
+});
+
+function parseGoalies(attributes, year, team) {
+    var saves = attributes[7];
+    var goalsConceded = attributes[8];
+    var fName = attributes[2];
+    var lName = attributes[3];
+    var year = year;
+    var goalieTuple = 'INSERT INTO GOALIES VALUES ('
+        +saves+','
+        +goalsConceded+','
+        +fName+','
+        +lName+','
+        +year+');';
+    var playerTuple = parseGoaliePlayer(attributes, year, team);
+    return [goalieTuple, playerTuple];
+}
+function parseGoaliePlayer(attributes, year, team) {
+    var starts = attributes[5];
+    var subIns = attributes[6];
+    var age = attributes[4];
+    var foulsCommitted = attributes[9];
+    var yellowCards = attributes[11];
+    var redCards = attributes[12];
+    var foulsAgainst = attributes[10];
+    var isGoalie = 'TRUE';
+    var currTeam = team;
+    var fName = attributes[2];
+    var lName = attributes[3];
+    var year = year;
+    var tuple = 'INSERT INTO PLAYERS VALUES ('
+        +fName+','
+        +lName+','
+        +year+','
+        +starts+','
+        +subIns+','
+        +age+','
+        +foulsCommitted+','
+        +yellowCards+','
+        +redCards+','
+        +foulsAgainst+','
+        +isGoalie+','
+        +currTeam+');';
+    return tuple;
+}
+
+function parseOutfielders(attributes, year, team) {
+    var goals = attributes[7]
+    var shotsOnGoal = attributes[9];
+    var shots = attributes[8];
+    var assists = attributes[10];
+    var position = attributes[0];
+    var fName = attributes[2];
+    var lName = attributes[3];
+    var year = year;
+    var outfielderTuple = 'INSERT INTO OUTFIELDERS VALUES ('
+        +goals+','
+        +shots+','
+        +shotsOnGoal+','
+        +assists+','
+        +position+','
+        +fName+','
+        +lName+','
+        +year+');'; 
+    var playerTuple = parseOutfielderPlayer(attributes, year, team); 
+    return [outfielderTuple, playerTuple];
+}
+
+function parseOutfielderPlayer(attributes, year, team) {
+    var starts = attributes[5];
+    var subIns = attributes[6];
+    var age = attributes[4];
+    var foulsCommitted = attributes[11];
+    var yellowCards = attributes[13];
+    var redCards = attributes[14];
+    var foulsAgainst = attributes[12];
+    var isGoalie = 'FALSE';
+    var currTeam = team;
+    var fName = attributes[2];
+    var lName = attributes[3];
+    var year = year;
+    var tuple = 'INSERT INTO PLAYERS VALUES ('
+        +fName+','
+        +lName+','
+        +year+','
+        +starts+','
+        +subIns+','
+        +age+','
+        +foulsCommitted+','
+        +yellowCards+','
+        +redCards+','
+        +foulsAgainst+','
+        +isGoalie+','
+        +currTeam+');';
+    return tuple;
+}
+
+casper.run();
